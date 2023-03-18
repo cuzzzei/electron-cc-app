6e2d3223a8c58082737a6aec210ee2f057949b81
Christian Velez Medina <christian.gerardo_@outlook.es>
2023-03-18 14:19:58 -0600

    feat: delete all agents

--- packages/renderer/src/features/agents/components/AgentsSettings.tsx
+++ packages/renderer/src/features/agents/components/AgentsSettings.tsx
@@ -1,14 +1,37 @@
+import { Button } from '/@/components/Button'
 import { Cog8ToothIcon } from '@heroicons/react/24/outline'
 import { Modal } from '/@/components/Modal'
+import { useAppContext } from '/@/providers/app'
 import { useState } from 'react'
+import { useToast } from '/@/hooks/useToast'
 
 export const AgentsSettings = () => {
+   const toast = useToast()
+   const { agentList, render } = useAppContext()
    const [isOpen, setIsOpen] = useState(false)
+   const [showDelete, setShowDelete] = useState(false)
+
+   function handleClose() {
+      setIsOpen(false)
+      setShowDelete(false)
+   }
+
+   function handleDelete() {
+      agentList.deleteAll()
+      render()
+
+      toast({
+         title: 'All agents have been deleted',
+         status: 'success',
+      })
+
+      setShowDelete(false)
+   }
 
    return (
       <Modal
          isOpen={isOpen}
-         onClose={() => setIsOpen(false)}
+         onClose={() => handleClose()}
          title='Agents settings'
          triggerButton={
             <div
@@ -20,7 +43,41 @@ export const AgentsSettings = () => {
             </div>
          }
       >
-         <p>hello world</p>
+         <div
+            style={{ height: '40px' }}
+            className='d-flex align-items-start mt-5'
+         >
+            {showDelete ? (
+               <div>
+                  <p className='m-0'>
+                     Are you sure you want to <b>delete all agents?</b>
+                  </p>
+
+                  <div className='d-flex gap-2 mt-3'>
+                     <Button
+                        colorScheme='gray'
+                        onClick={() => setShowDelete(false)}
+                     >
+                        Cancel
+                     </Button>
+
+                     <Button
+                        colorScheme='red'
+                        onClick={() => handleDelete()}
+                     >
+                        Yes, I am sure
+                     </Button>
+                  </div>
+               </div>
+            ) : (
+               <Button
+                  colorScheme='red'
+                  onClick={() => setShowDelete(true)}
+               >
+                  Delete all agents
+               </Button>
+            )}
+         </div>
       </Modal>
    )
 }
