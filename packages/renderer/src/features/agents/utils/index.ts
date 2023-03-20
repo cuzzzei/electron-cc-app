857994246d17dab6b1fad094bb215d2109ccc708
Christian Velez Medina <christian.gerardo_@outlook.es>
2023-03-20 10:25:15 -0600

    test data

---
+++ packages/renderer/src/features/agents/utils/index.ts
@@ -0,0 +1,79 @@
+import { Agent, AgentList, specialties } from '/@/features/agents'
+import { Call, CallList } from '/@/features/calls'
+import { faker } from '@faker-js/faker'
+import { Name } from '/@/types/Name'
+import { Time } from '/@/types/Time'
+import { v4 as uuid } from 'uuid'
+
+function randomNumber(min: number, max: number): number {
+   return faker.datatype.number({
+      min,
+      max,
+   })
+}
+
+function randomTime(): [Time, Time] {
+   const startHour = randomNumber(7, 12)
+   const startMinute = faker.helpers.arrayElement([0, 15, 30, 45])
+
+   const startTime = new Time(startHour, startMinute)
+   const endTime = new Time(startHour + 7, startMinute)
+
+   return [startTime, endTime]
+}
+
+function createRandomCall(start: Time, end: Time): Call {
+   const call = new Call()
+   call.setId(uuid())
+   call.setClientName(new Name(faker.name.firstName(), faker.name.lastName()))
+
+   call.setStartTime(
+      new Time(
+         randomNumber(start.getHour(), end.getHour() - 1),
+         randomNumber(0, 50)
+      )
+   )
+   call.setDuration(new Time(0, randomNumber(1, 25)))
+
+   return call
+}
+
+export function createRandomAgent(): Agent {
+   const agent = new Agent()
+   agent.setId(uuid())
+   agent.setExtension(randomNumber(1111, 9999).toString())
+   agent.setName(new Name(faker.name.firstName(), faker.name.lastName()))
+   agent.setAge(randomNumber(18, 40))
+   agent.setCallHistory(new CallList())
+   agent.setOvertime(randomNumber(0, 3))
+   agent.setSpecialty(faker.helpers.arrayElement(specialties))
+   const [start, end] = randomTime()
+   agent.setStartTime(start)
+   agent.setEndTime(end)
+
+   for (let i = 0; i < 30; i++) {
+      agent
+         .getCallHistory()
+         .insertOrdered(
+            createRandomCall(agent.getStartTime(), agent.getEndTime()),
+            Call.compareByStartTime
+         )
+   }
+
+   return agent
+}
+
+export function createAgentList() {
+   const list = new AgentList()
+
+   for (let i = 0; i < 11; i++) {
+      const newagent = createRandomAgent()
+      list.insertAtEnd(newagent)
+   }
+
+   const final = {
+      agents: list.toJSON(),
+   }
+
+   console.log(JSON.stringify(final))
+}
--- packages/renderer/src/providers/app.tsx
+++ packages/renderer/src/providers/app.tsx
@@ -1,13 +1,7 @@
-import { Agent, AgentList } from '/@/features/agents'
-import { CallList } from '/@/features/calls'
-import { faker } from '@faker-js/faker'
+import { AgentList } from '/@/features/agents'
 import { HashRouter } from 'react-router-dom'
-import { Name } from '/@/types/Name'
-import { specialties } from '../features/agents/types/Specialty'
-import { Time } from '/@/types/Time'
 import { ToastProvider } from '/@/providers/ToastProvider'
 import { useRender } from '/@/hooks'
-import { v4 as uuid } from 'uuid'
 import React, { createContext, useContext, useRef } from 'react'
 
 interface AppContextType {
@@ -44,8 +38,6 @@ export const AppProvider = ({
 }) => {
    const context = useInitAppContext(initialValues?.agentList)
 
-   createRandomAgent()
-
    return (
       <HashRouter>
          <AppContext.Provider value={context}>
@@ -53,40 +45,4 @@ export const AppProvider = ({
          </AppContext.Provider>
       </HashRouter>
    )
-}
-
-function randomNumber(min: number, max: number): number {
-   return faker.datatype.number({
-      min,
-      max,
-   })
-}
-
-function randomTime(): [Time, Time] {
-   const startHour = randomNumber(7, 12)
-   const startMinute = faker.helpers.arrayElement([0, 15, 30, 45])
-
-   const startTime = new Time(startHour, startMinute)
-   const endTime = new Time(startHour + 7, startMinute)
-
-   return [startTime, endTime]
-}
-
-function createRandomAgent(): Agent {
-   const agent = new Agent()
-   agent.setId(uuid())
-   agent.setExtension(randomNumber(1111, 9999).toString())
-   agent.setName(new Name(faker.name.firstName(), faker.name.lastName()))
-   agent.setAge(randomNumber(18, 40))
-   agent.setCallHistory(new CallList())
-   agent.setOvertime(randomNumber(0, 3))
-   agent.setSpecialty(faker.helpers.arrayElement(specialties))
-
-   const [start, end] = randomTime()
-
-   agent.setStartTime(start)
-   agent.setEndTime(end)
-
-   console.log(agent.toJSON())
-   return agent
 }
